3e5596b624e1ce90d4d1a78b0c3c9bc9542800f8
ram7950 <ramtheking.yadav1995@gmail.com>
2026-04-30 05:54:52 +0530

    Patch 3.1.0b fix signing (no uninstall updates)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,52 +1,54 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.rambo.ramcryptr"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rambo.ramcryptr"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 4
-        versionName = "2.0"
+        targetSdk = 34
+        versionCode = 6
+        versionName = "3.1.0b"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildToolsVersion = "33.0.2"
+    signingConfigs {
+        create("release") {
+            storeFile = file("ramcryptr.keystore")
+            storePassword = "123456"
+            keyAlias = "ramcryptr"
+            keyPassword = "123456"
+        }
+    }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+        }
+
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
     compileOptions {
-        sourceCompatibility =
-            JavaVersion.VERSION_17
-        targetCompatibility =
-            JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget="17"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-
-implementation(
-"androidx.core:core-ktx:1.10.1"
-)
-
-implementation(
-"androidx.appcompat:appcompat:1.6.1"
-)
-
-implementation(
-"com.google.android.material:material:1.9.0"
-)
-
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 }
