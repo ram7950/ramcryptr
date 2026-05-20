bd0ffa4569edc119ec592039ba96f03b56abd595
ram7950 <ramtheking.yadav1995@gmail.com>
2026-05-20 05:54:33 +0530

    Add compressed intro video and ExoPlayer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+
+    implementation("androidx.media3:media3-exoplayer:1.3.1")
+    implementation("androidx.media3:media3-ui:1.3.1")
 }
 
 configurations.all {
